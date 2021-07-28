06b3003642fb14ae8cc7ecd755d0cc146a9baf9c
Rafael <rafael.2018.ads@gmail.com>
2021-07-28 18:49:33 -0300

    Configurando e exibindo HTML com EJS

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('App rodando.');
+});
