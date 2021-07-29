ec629f1598dee3252bb7c456226574b3aac7b4db
Rafael <rafael.2018.ads@gmail.com>
2021-07-29 16:25:37 -0300

    Arquivos estaticos

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/src/views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
